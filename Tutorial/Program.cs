675205f006d752f4b47367c2e68c1195d4218e2e
Hannes Kasel <hanneskasel@gmail.com>
2022-10-09 14:31:06 +0200

    Strukturelle und OOP Änderungen

--- Tutorial/Program.cs
+++ Tutorial/Program.cs
@@ -13,6 +13,15 @@ namespace Analyzer.app
         static void Main()
         {
             IPLogic ip = new IPLogic();
+
+            //Console Output
+            Console.WriteLine($"IP Adresse:\t\t{ip.IPAdresse[0]}.{ip.IPAdresse[1]}.{ip.IPAdresse[2]}.{ip.IPAdresse[3]}");
+            Console.WriteLine($"Subnetzmaske:\t\t{ip.Subnetzmaske[0]}.{ip.Subnetzmaske[1]}.{ip.Subnetzmaske[2]}.{ip.Subnetzmaske[3]}");
+            Console.WriteLine($"Netzwerkadresse:\t{ip.NetzwerkAdresse[0]}.{ip.NetzwerkAdresse[1]}.{ip.NetzwerkAdresse[2]}.{ip.NetzwerkAdresse[3]}");
+            Console.WriteLine($"Broadcastadresse:\t{ip.BroadcastAdresse[0]}.{ip.BroadcastAdresse[1]}.{ip.BroadcastAdresse[2]}.{ip.BroadcastAdresse[3]}");
+            Console.WriteLine($"Anzahl Hosts:\t\t{ip.AnzahlHosts}");
+            Console.WriteLine($"Anzahl Subnetze:\t{ip.AnzahlNachbarnetze}");
+            Console.ReadKey();
             Console.ReadKey();
            
             
